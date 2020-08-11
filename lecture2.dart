89f610533d42ab888dd953635f7d644c70e8543a
hetvi <hetvi.kothari@somaiya.edu>
2020-08-12 00:05:27 +0530

    Lecture 2

---
+++ lecture2.dart
@@ -0,0 +1,16 @@
+main() {
+ List<int> value = [1, 40, 10, 333, 50, 15, 77];
+  
+  int max = value[0];
+  
+  for(int i=0;i<value.length;i++)
+  {
+    if(value[i]>max)
+      max=value[i];
+  }
+  
+
+  print("Largest value :$max");
+  
+  
+}
